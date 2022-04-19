00a52b2177fe18c427267849ed2317486dd62b9e
sheylla-rodrigues <89166914+sheylla-rodrigues@users.noreply.github.com>
2022-04-19 18:55:53 -0300

    Add files via upload

---
+++ CalculadoraJava/src/Classes/Divisao.java
@@ -0,0 +1,53 @@
+package Classes;
+
+import Interface.ICalculadora;
+
+public class Divisao implements ICalculadora{
+
+	@Override
+	public Integer calcula(Integer n1, Integer n2) {
+		return n1/n2;
+	}
+
+	@Override
+	public Float calcula(Float n1, Float n2) {
+		return n1/n2;
+	}
+
+	@Override
+	public Double calcula(Double n1, Double n2) {
+		return n1/n2;
+	}
+
+	@Override
+	public Integer calcula(Integer[] numeros) {
+		int divisao = 0;
+		for (Integer d : numeros) {
+			divisao = d.intValue() / d.intValue();
+		}
+		return divisao;
+	}
+
+	@Override
+	public Float calcula(Float[] numeros) {
+		Float divisao = 0f;
+		for (Float d : numeros) {
+			divisao = d.floatValue() / 
+
+d.floatValue();
+		}
+		return divisao;
+	}
+
+	@Override
+	public Double calcula(Double[] numeros) {
+		Double divisao = 0d;
+		for (Double d : numeros) {
+			divisao = d.doubleValue() / 
+
+d.doubleValue();
+		}
+		return divisao;
+	}
+	
+}
